6025872a8fb056a60b867df81baaa77bd366f214
netcyphe <netcyphe@openmailbox.org>
2014-01-12 19:48:36 +0100

    Added license notice

--- easyfglrxinstall.sh
+++ easyfglrxinstall.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
+###########################################################################
+#                                                                         #
+#  easyfglrxinstall.sh - Install the fglrx driver on Ubuntu systems       #
+#  Copyright (C) 2013-2014 netcyphe <netcyphe@openmailbox.org>            #
+#                                                                         #
+#  This program is free software: you can redistribute it and/or modify   #
+#  it under the terms of the GNU General Public License as published by   #
+#  the Free Software Foundation, either version 3 of the License, or      #
+#  (at your option) any later version.                                    #
+#                                                                         #
+#  This program is distributed in the hope that it will be useful,        #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#  GNU General Public License for more details.                           #
+#                                                                         #
+#  You should have received a copy of the GNU General Public License      #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#                                                                         #
+###########################################################################
+
 ##################################
 #                                #
 # Easy fglrx installation script #
 #                                #
 ##################################
 
-# easyfglrxinstall.sh written by netcyphe - 01/06/2013
+# easyfglrxinstall.sh written by netcyphe - 01/12/2013
 
 # Accepted distro versions: gutsy, hardy, intrepid, jaunty, lucid, maverick, natty, oneiric, precise, quantal, raring
 # Not yet implemented distro versions: breezy, dapper, feisty

fae829f9d9d1b0eaaca67a0698a7e04680a840fb
Christian Heinrichs <christian.heinrichs@openmailbox.org>
2014-05-15 16:32:02 +0200

    - Updated README.md - Updated GNU license notice

--- easyfglrxinstall.sh
+++ easyfglrxinstall.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-###########################################################################
-#                                                                         #
-#  easyfglrxinstall.sh - Install the fglrx driver on Ubuntu systems       #
-#  Copyright (C) 2013-2014 netcyphe <netcyphe@openmailbox.org>            #
-#                                                                         #
-#  This program is free software: you can redistribute it and/or modify   #
-#  it under the terms of the GNU General Public License as published by   #
-#  the Free Software Foundation, either version 3 of the License, or      #
-#  (at your option) any later version.                                    #
-#                                                                         #
-#  This program is distributed in the hope that it will be useful,        #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of         #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
-#  GNU General Public License for more details.                           #
-#                                                                         #
-#  You should have received a copy of the GNU General Public License      #
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
-#                                                                         #
-###########################################################################
+#######################################################################################
+#                                                                                     #
+#  easyfglrxinstall.sh - Install the fglrx driver on Ubuntu systems                   #
+#  Copyright (C) 2013-2014 Christian Heinrichs <christian.heinrichs@openmailbox.org>  #
+#                                                                                     #
+#  This program is free software: you can redistribute it and/or modify               #
+#  it under the terms of the GNU General Public License as published by               #
+#  the Free Software Foundation, either version 3 of the License, or                  #
+#  (at your option) any later version.                                                #
+#                                                                                     #
+#  This program is distributed in the hope that it will be useful,                    #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+#  GNU General Public License for more details.                                       #
+#                                                                                     #
+#  You should have received a copy of the GNU General Public License                  #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.              #
+#                                                                                     #
+#######################################################################################
 
 ##################################
 #                                #
@@ -26,7 +26,7 @@
 #                                #
 ##################################
 
-# easyfglrxinstall.sh written by netcyphe - 01/25/2014
+# easyfglrxinstall.sh written by Christian Heinrichs - 05/15/2014
 
 # Accepted distro versions: gutsy, hardy, intrepid, jaunty, lucid, maverick, natty, oneiric, precise, quantal, raring
 # Not yet implemented distro versions: breezy, dapper, feisty
